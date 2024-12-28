1ef7d19efee35ca781f64b4335463ad0ae19f0b3
Tirza Gabriella <tirzagabriella20@gmail.com>
2024-12-28 15:15:38 +1100

    change color

--- input_box.py
+++ input_box.py
@@ -1,8 +1,8 @@
 import pygame as pg
 
 pg.init()
-COLOR_INACTIVE = pg.Color('lightskyblue3')
-COLOR_ACTIVE = pg.Color('dodgerblue2')
+COLOR_INACTIVE = pg.Color('aqua')
+COLOR_ACTIVE = pg.Color('aliceblue')
 FONT = pg.font.Font(None, 32)
 
 class InputBox:
